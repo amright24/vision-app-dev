b54cdc17c91d1e8c20b6c2001059c53d8ff53551
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-03 12:13:01 -0400

    first commit

---
+++ vision-app-dev/View/RoundedShadowImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedShadowImageView.swift
+//  vision-app-dev
+//
+//  Created by Austin Rightnowar on 4/3/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
