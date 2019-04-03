5fce658cb2095707d3c6424310125fd4cb01e6a6
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-03 10:32:04 -0400

    Initial Commit

---
+++ vision-app-dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by Austin Rightnowar on 4/3/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
