aabf424f36c99ad9e1dfefd2a621ee3e40a662b5
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-04 11:50:19 -0400

    before trying thins out

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -7,14 +7,93 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
-
+    
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var photoData = Data?.self
+    
+    // O U T L E T S
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var captureImageView: RoundedShadowImageView!
+    @IBOutlet weak var flashBtn: RoundedShadowButton!
+    @IBOutlet weak var identificationLbl: UILabel!
+    @IBOutlet weak var confidenceLbl: UILabel!
+    @IBOutlet weak var roundedLblView: RoundedShadowView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupDevice()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+//        tap.numberOfTapsRequired = 1
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        func setupDevice() {
+            let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) == true {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(cameraOutput) == true {
+                captureSession.addOutput(cameraOutput!)
+            }
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+            
+            cameraView.layer.addSublayer(previewLayer!)
+            captureSession.startRunning()
+        } catch {
+            debugPrint(error)
+        }
+    }
+//    @objc func didTapCameraView() {
+//        let settings = AVCapturePhotoSettings()
+//        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+//        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+//
+//        settings.previewPhotoFormat = previewFormat
+//
+//        cameraOutput.capturePhoto(with: settings, delegate: self)
+    
 
 
 }
 
+//extension CameraVC: AVCapturePhotoCaptureDelegate {
+//        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+//            if let error = error {
+//                debugPrint(error)
+//            } else {
+//                photoData = photo.fileDataRepresentation()
+//
+//                let image = UIImage(data: photoData!)
+//                self.captureImageView.image = image
+//            }
+//    }
+//}
--- vision-app-dev/View/RoundedShadowImageView.swift
+++ vision-app-dev/View/RoundedShadowImageView.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
+
 class RoundedShadowImageView: UIImageView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 15
+        self.layer.shadowOpacity = 0.75
+        self.layer.cornerRadius = 15
     }
-    */
 
 }
