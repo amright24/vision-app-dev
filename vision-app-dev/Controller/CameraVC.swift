79e2243a2982ec481e5f35e27914dd99ee21434d
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-05 12:54:07 -0400

    its all working and we're at the point to where the app takes a picture uses coreml to guess what it is and gives us a confidence interval as well as uses the flash

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
+
+enum FlashState {
+    case off
+    case on
+}
 
 class CameraVC: UIViewController {
+    
+    var flashControlState: FlashState = .off
     
     var captureSession = AVCaptureSession()
     var backCamera: AVCaptureDevice?
     var frontCamera: AVCaptureDevice?
     var currentCamera: AVCaptureDevice?
-    
     var photoOutput: AVCapturePhotoOutput?
-    
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var photoData: Data?
     
     // O U T L E T S
     @IBOutlet weak var cameraView: UIView!
@@ -31,11 +40,6 @@ class CameraVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupCaptureSession()
-        setupDevice()
-        setupInputOutput()
-        setupPreviewLayer()
-        startRunningCaptureSession()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -44,17 +48,12 @@ class CameraVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-    }
-    
-    func setupCaptureSession() {
+        captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
-    }
-    
-    func setupDevice() {
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         let devices = deviceDiscoverySession.devices
-        
+        var tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
         for device in devices {
             if device.position == AVCaptureDevice.Position.back {
                 backCamera = device
@@ -62,40 +61,101 @@ class CameraVC: UIViewController {
                 frontCamera = device
             }
         }
-        
         currentCamera = backCamera
-    }
     
-    
-    
-    func setupInputOutput() {
         do {
             let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
             captureSession.addInput(captureDeviceInput)
             photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+            
+            
+            photoOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddOutput(photoOutput!) == true {
+                captureSession.addOutput(photoOutput!)
+                
+                cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                cameraPreviewLayer?.frame = cameraView.bounds
+                
+                cameraView.layer.addSublayer(cameraPreviewLayer!)
+                cameraView.addGestureRecognizer(tap)
+                captureSession.startRunning()
+            }
         } catch {
             print(error)
         }
+    }
+  
+    @objc func didTapCameraView(sender: UITapGestureRecognizer) {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.__availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
         
-   }
-    
-    func setupPreviewLayer() {
-        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-        cameraPreviewLayer?.frame = cameraView.bounds
-        self.cameraView.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        settings.previewPhotoFormat = previewFormat
+        
+        if flashControlState == .off {
+            settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        
+        photoOutput?.capturePhoto(with: settings, delegate: self)
     }
     
-    func startRunningCaptureSession() {
-        captureSession.startRunning()
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            if classification.confidence < 0.5 {
+                print(classification.identifier)
+                self.identificationLbl.text = "I'm not sure what this is, try again!"
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                self.identificationLbl.text = classification.identifier
+                self.confidenceLbl.text = "CONFIDENCE: \(Int(classification.confidence * 100))%"
+                break
+            }
+        }
+    }
+    
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashControlState {
+        case .off:
+            flashBtn.setTitle("FLASH ON", for: .normal)
+            flashControlState = .on
+        case .on:
+            flashBtn.setTitle("FLASH OFF", for: .normal)
+            flashControlState = .off
+        }
     }
     
     
+}
 
-    
-
-
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+            } catch {
+                debugPrint(error)
+            }
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+            print("did take photo")
+        }
+    }
 }
 
 
